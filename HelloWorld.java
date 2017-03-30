1468feebd5d1cd977ef7115cb40098ad90fbb158
Victor Gallegos <victor.gallegos@softtek.com>
2017-03-29 21:57:49 -0600

    4o commit - 'Hello Git and GitHub'

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git a bug was fixed!!");
+		System.out.println("Hello Git and GitHub, a bug was fixed!!");
 	}
-
 }
